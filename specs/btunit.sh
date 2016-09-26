178909acc7b95bb25f2173cf6a87f83825d58374
Daniel Krizian <daniel.krizian@gmail.com>
2016-09-26 10:38:54 +0100

    set better ConEmu working layout

--- specs/btunit.sh
+++ specs/btunit.sh
@@ -15,11 +15,11 @@ echo "Starting oms ..."
 q torq.q -load ${KDBCODE}/processes/oms.q ${KDBSTACKID} -trap -proctype oms -procname oms1 -localtime -debug -new_console:sHnc:t:'oms' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
 
 echo "Starting fillsim ..."
-q torq.q -load ${KDBCODE}/processes/fillsim.q ${KDBSTACKID} -trap -proctype fillsim -procname fillsim1 -localtime -debug -new_console:sVnc:t:'fillsim' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
+q torq.q -load ${KDBCODE}/processes/fillsim.q ${KDBSTACKID} -trap -proctype fillsim -procname fillsim1 -localtime -debug -new_console:s8TV:t:'fillsim' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
 
 echo "Starting portfolio ..."
-q torq.q -load ${KDBCODE}/processes/portfolio.q ${KDBSTACKID} -trap -proctype portfolio -procname portfolio1 -localtime -debug -new_console:sHnc:t:'portfolio' #</dev/null >$KDBLOG/gateway.txt 2>&1 &
+q torq.q -load ${KDBCODE}/processes/portfolio.q ${KDBSTACKID} -trap -proctype portfolio -procname portfolio1 -localtime -debug -new_console:s9TV:t:'portfolio' #</dev/null >$KDBLOG/gateway.txt 2>&1 &
 
 echo "Starting feed ..."
-q torq.q -load code/tick/backtestfeed.q ${KDBSTACKID} -trap -proctype backtestfeed -procname backtestfeed1 -localtime -tbls mtm signal -bgn 2016.05.02 -end 2016.05.31 -syms AAPL PRU GOOG MSFT -debug -new_console:sVnc:t:'btfeed' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 & # -bgn 2016.05.02 -end 2016.05.02 -syms GOOG IBM MSFT
+q torq.q -load code/tick/backtestfeed.q ${KDBSTACKID} -trap -proctype backtestfeed -procname backtestfeed1 -localtime -tbls mtm signal -bgn 2016.05.02 -end 2016.05.31 -syms AAPL PRU GOOG MSFT -debug -new_console:s4TV:t:'btfeed' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 & # -bgn 2016.05.02 -end 2016.05.02 -syms GOOG IBM MSFT
 
