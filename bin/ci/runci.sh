9dfc1a5aa725e90916e561d327c9939e1e149bb4
Morten Sorensen <samsogade@gmail.com>
2016-10-10 22:32:31 +0800

    Clean up CI files

--- bin/ci/runci.sh
+++ bin/ci/runci.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# source ${PROJECT_DIR}/bin/setenv.sh
-# testq tests/test_test.q --junit | sed -n '/<testsuites>$/,$p'
 
 source bin/setenv.sh
 testq tests/test_test.q --junit | sed -n '/<testsuites>$/,$p'
