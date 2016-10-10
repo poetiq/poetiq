f1e70d43aad84eef8ed6dee7d21f5af06578243e
Daniel Krizian <daniel.krizian@gmail.com>
2016-10-10 15:16:18 +0100

    fix previous commit
    delete APPCONFIG default settings; revert process order,

--- bin/btunit.sh
+++ bin/btunit.sh
@@ -11,15 +11,14 @@ q torq.q -load ${KDBCODE}/processes/gateway.q ${KDBSTACKID} -proctype gateway -p
 echo "Starting optimiser ..."
 q torq.q -load ${KDBCODE}/processes/optimiser.q ${KDBSTACKID} -trap -proctype optimiser -procname optimiser1 -localtime -debug -new_console:nc:t:'optimiser' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
 
-echo "Starting portfolio ..."
-q torq.q -load ${KDBCODE}/processes/portfolio.q ${KDBSTACKID} -trap -proctype portfolio -procname portfolio1 -localtime -debug -new_console:sHnc:t:'portfolio' -e 1 -.clients.enabled 0 -.usage.enabled 0 #</dev/null >$KDBLOG/gateway.txt 2>&1 &
-
 echo "Starting oms ..."
-q torq.q -load ${KDBCODE}/processes/oms.q ${KDBSTACKID} -trap -proctype oms -procname oms1 -localtime -debug -new_console:s8TV:t:'oms' -e 1 -.clients.enabled 0 -.usage.enabled 0 # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
+q torq.q -load ${KDBCODE}/processes/oms.q ${KDBSTACKID} -trap -proctype oms -procname oms1 -localtime -debug -new_console:sHnc:t:'oms' -e 1 -.clients.enabled 0 -.usage.enabled 0 # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
 
 echo "Starting fillsim ..."
 q torq.q -load ${KDBCODE}/processes/fillsim.q ${KDBSTACKID} -trap -proctype fillsim -procname fillsim1 -localtime -debug -new_console:s8TV:t:'fillsim' -e 1 -.clients.enabled 0 -.usage.enabled 0 # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
 
-echo "Starting feed ..."
-q torq.q -load ${KDBCODE}/tick/backtestfeed.q ${KDBSTACKID} -wait -trap -proctype backtestfeed -procname backtestfeed1 -localtime -tbls mtm signal -bgn 2016.05.02 -end 2016.05.31 -syms AAPL PRU GOOG MSFT -debug -new_console:s9TV:t:'btfeed' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 & # -bgn 2016.05.02 -end 2016.05.02 -syms GOOG IBM MSFT
+echo "Starting portfolio ..."
+q torq.q -load ${KDBCODE}/processes/portfolio.q ${KDBSTACKID} -trap -proctype portfolio -procname portfolio1 -localtime -debug -new_console:s9TV:t:'portfolio' -e 1 -.clients.enabled 0 -.usage.enabled 0 #</dev/null >$KDBLOG/gateway.txt 2>&1 &
 
+echo "Starting feed ..."
+q torq.q -load ${KDBCODE}/tick/backtestfeed.q ${KDBSTACKID} -wait -trap -proctype backtestfeed -procname backtestfeed1 -localtime -tbls mtm signal -bgn 2016.05.02 -end 2016.05.31 -syms AAPL PRU GOOG MSFT -debug -new_console:s4TV:t:'btfeed' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 & # -bgn 2016.05.02 -end 2016.05.02 -syms GOOG IBM MSFT
