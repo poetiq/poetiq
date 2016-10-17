7178943e6628481980bb84cb23ca223ae8c6e97a
Daniel Krizian <daniel.krizian@gmail.com>
2016-10-17 17:13:08 +0100

    start_poetiq, btunit made working with startp
    - startp in start_poetiq.sh and btunit.sh
    - process args delegated to start.csv
    - hdb now sibling to poetiq dir
    - oms doesn't need to load hdb

--- bin/btunit.sh
+++ bin/btunit.sh
@@ -1,38 +1,17 @@
 # Run each of these commands in a separate window during debugging
 
-source ./setenv.sh
+source bin/control.sh
 
-echo "Starting hdb ..."
-q torq.q -load sandbox/datalab.q ${KDBSTACKID} -proctype hdb -procname equitysim -localtime -g 1 -T 60 -w 4000 -debug -new_console:nc:t:'hdb' #</dev/null >$KDBLOG/equitysim.txt 2>&1 &
-
-echo "Starting gateway ..."
-q torq.q -load ${KDBCODE}/processes/gateway.q ${KDBSTACKID} -proctype gateway -procname gateway1 -.servers.CONNECTIONS hdb rdb -localtime -g 1 -w 4000 -debug -new_console:sVnc:t:'gateway' #</dev/null >$KDBLOG/gateway.txt 2>&1 &
-
-echo "Starting optimiser ..."
-q torq.q -load ${KDBCODE}/processes/optimiser.q ${KDBSTACKID} -trap -proctype optimiser -procname optimiser1 -localtime -debug -new_console:nc:t:'optimiser' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
-
-echo "Starting oms ..."
-q torq.q -load ${KDBCODE}/processes/oms.q ${KDBSTACKID} -trap -proctype oms -procname oms1 -localtime -debug -new_console:sHnc:t:'oms' -e 1 -.clients.enabled 0 -.usage.enabled 0 # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
-
-echo "Starting fillsim ..."
-q torq.q -load ${KDBCODE}/processes/fillsim.q ${KDBSTACKID} -trap -proctype fillsim -procname fillsim1 -localtime -debug -new_console:s8TV:t:'fillsim' -e 1 -.clients.enabled 0 -.usage.enabled 0 # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 &
-
-echo "Starting portfolio ..."
-q torq.q -load ${KDBCODE}/processes/portfolio.q ${KDBSTACKID} -trap -proctype portfolio -procname portfolio1 -localtime -debug -new_console:s9TV:t:'portfolio' -e 1 -.clients.enabled 0 -.usage.enabled 0 #</dev/null >$KDBLOG/gateway.txt 2>&1 &
-
-echo "Starting feed ..."
-q torq.q -load ${KDBCODE}/tick/backtestfeed.q ${KDBSTACKID} -wait -trap -proctype backtestfeed -procname backtestfeed1 -localtime -tbls mtm signal -bgn 2016.05.02 -end 2016.05.31 -syms AAPL PRU GOOG MSFT -debug -new_console:s4TV:t:'btfeed' # </dev/null >$KDBLOG/bttickerplant.txt 2>&1 & # -bgn 2016.05.02 -end 2016.05.02 -syms GOOG IBM MSFT
-
-
-
-# Proposed new setup
-
-# source bin/control.sh
-
-# startp hdb equitysim 							--debug --args -new_console:nc:t:'hdb'
-# startp gateway gateway1						--debug --args -new_console:sVnc:t:'gateway'
-# startp optimiser optimiser1				--debug --args -new_console:nc:t:'optimiser'
-# startp oms oms1										--debug --args -new_console:sHnc:t:'oms'
-# startp fillsim fillsim1						--debug --args -new_console:s8TV:t:'fillsim'
-# startp portfolio portfolio1				--debug --args -new_console:s9TV:t:'portfolio'
-# startp backtestfeed backtestfeed1	--debug --args -new_console:s4TV:t:'btfeed' -wait -trap -tbls mtm signal -bgn 2016.05.02 -end 2016.05.31 -syms AAPL PRU GOOG MSFT
+startp hdb equitysim 					--debug
+cd ${POETIQ}
+startp gateway gateway1					--debug
+cd ${POETIQ}
+startp optimiser optimiser1				--debug
+cd ${POETIQ}
+startp oms oms1							--debug
+cd ${POETIQ}
+startp fillsim fillsim1					--debug
+cd ${POETIQ}
+startp portfolio portfolio1				--debug
+cd ${POETIQ}
+startp backtestfeed backtestfeed1		--debug
--- src/oms/o.q
+++ src/oms/o.q
@@ -1,4 +1,3 @@
-\l hdb/equitysim
 
 upd: ()!()
 
