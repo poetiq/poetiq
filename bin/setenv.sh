4c747c593f158a85a914a517b03adc3cb384c73a
Morten Sorensen <samsogade@gmail.com>
2016-10-18 21:42:17 +0800

    Clean up setenv.sh

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -6,19 +6,17 @@ export QBIN=$(find $QHOME -type f \( -name q.exe -or -name q \) | head -n 1)
 
 export POETIQ="$(dirname $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd))"
 export KDBCODE=${POETIQ}/src
+export KDBBIN=${POETIQ}/bin
 export KDBCONFIG=${POETIQ}/config
 export KDBLOG=${POETIQ}/logs
-export KDBHDB=../hdb
+export KDBHDB=${POETIQ}/hdb
 export KDBLIB=${POETIQ}/lib
 export KDBTESTS=${POETIQ}/tests
-export KDBSPECS=${POETIQ}/specs
-export KDBHTML=${POETIQ}/html
+export TORQ=${POETIQ}/torq.q
 
 export KDBBASEPORT=5000
 
 export KDBSTACKID="-stackid ${KDBBASEPORT}"
-
-cd ${POETIQ}
 
 
 # if using the email facility, modify the library path for the email lib depending on OS
