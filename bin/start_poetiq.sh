ad9ad5d246686832bcf1436803b3aaf57b77288f
Morten Sorensen <samsogade@gmail.com>
2016-10-18 21:43:08 +0800

    Move process specific args from start.csv to .sh files

--- bin/start_poetiq.sh
+++ bin/start_poetiq.sh
@@ -2,7 +2,7 @@
 
 source bin/control.sh
 
-startp discovery discovery1				--debug
-startp housekeeping housekeeping1		--debug
-startp btctrl btctrl1 					--debug
-startp bttickerplant bttickerplant1		--debug
+startp discovery discovery1					--debug --args -new_console:nc:t:'discovery'
+startp housekeeping housekeeping1		--debug --args -new_console:sVnc:t:'housekeeping'
+startp btctrl btctrl1 							--debug --args -e 1 -.clients.enabled 0 -.usage.enabled 0 -new_console:s2THnc:t:'btctrl'
+startp bttickerplant bttickerplant1	--debug --args -e 1 -c 25 200 -new_console:nc:t:'tickerplant'
